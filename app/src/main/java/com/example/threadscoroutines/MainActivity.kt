256cd79df5acc46e1a9171075262eb76bcc9959d
Italo Silva de Assunção <italo10sil@gmail.com>
2025-09-09 21:55:00 -0300

    feat: Add initial project files
    This commit introduces the basic structure of the Android application, including:
    - Gradle build files (`build.gradle.kts`, `settings.gradle.kts`, `gradle.properties`, `gradle-wrapper.properties`, `gradlew`, `gradlew.bat`)
    - `.gitignore` files for the project and app module
    - `AndroidManifest.xml`
    - Basic layout files for `MainActivity` and `SegundaActivity`
    - Corresponding Kotlin files for `MainActivity` and `SegundaActivity`
    - Default resource files (colors, strings, themes, drawables, mipmaps)
    - Basic unit and instrumented tests
    - Proguard rules
    - Backup and data extraction rules

---
+++ app/src/main/java/com/example/threadscoroutines/MainActivity.kt
@@ -0,0 +1,40 @@
+package com.example.threadscoroutines
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.example.threadscoroutines.R
+import com.example.threadscoroutines.SegundaActivity
+import com.example.threadscoroutines.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private val binding by lazy {
+        ActivityMainBinding.inflate(layoutInflater)
+    }
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+        setContentView(R.layout.activity_main)
+
+        binding.buttonNT.setOnClickListener {
+            startActivity(
+                Intent(this, SegundaActivity::class.java)
+            )
+        }
+
+        binding.buttonIniciar.setOnClickListener {
+
+        }
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+    }
+}

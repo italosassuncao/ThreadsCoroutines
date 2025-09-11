2899f10d0b7fb9b950bcea9d980e10c9f479d438
Italo Silva de Assunção <italo10sil@gmail.com>
2025-09-10 22:07:29 -0300

    feat: Implement basic threading and add IDE configuration files
    This commit introduces a simple threading example in `MainActivity.kt` using a custom `MinhaThread` class that extends `Thread`. It logs messages with the current thread name.
    
    Additionally, this commit includes several Android Studio IDE configuration files within the `.idea` directory. These files manage project settings related to:
    - Android project system (`AndroidProjectSystem.xml`)
    - Compiler configuration (`compiler.xml`)
    - Deployment target selection (`deploymentTargetSelector.xml`)
    - Gradle settings (`gradle.xml`)
    - Project migrations (`migrations.xml`)
    - General project settings like JDK and project type (`misc.xml`)
    - Run configurations (`runConfigurations.xml`)
    - Version control system mappings (`vcs.xml`)

--- app/src/main/java/com/example/threadscoroutines/MainActivity.kt
+++ app/src/main/java/com/example/threadscoroutines/MainActivity.kt
@@ -2,6 +2,7 @@ package com.example.threadscoroutines
 
 import android.content.Intent
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
@@ -9,6 +10,8 @@ import androidx.core.view.WindowInsetsCompat
 import com.example.threadscoroutines.R
 import com.example.threadscoroutines.SegundaActivity
 import com.example.threadscoroutines.databinding.ActivityMainBinding
+import java.lang.Thread.currentThread
+import java.lang.Thread.sleep
 
 class MainActivity : AppCompatActivity() {
 
@@ -28,13 +31,29 @@ class MainActivity : AppCompatActivity() {
         }
 
         binding.buttonIniciar.setOnClickListener {
+            MinhaThread().start()
+            /*repeat(15) { indice ->
+                Log.i("TAG", "onCreate: $indice T: ${currentThread().name}")
+                sleep(1000)
+            }*/
 
+            ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+                val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+                v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+                insets
+            }
         }
+    }
 
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
-            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
-            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
-            insets
+    inner class MinhaThread : Thread() {
+        override fun run() {
+            super.run()
+
+            repeat(30) { indice ->
+                Log.i("TAG", "onCreate: $indice T: ${currentThread().name}")
+                sleep(1000)
+
+            }
         }
     }
 }
